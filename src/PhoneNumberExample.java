f51b32bc8e40c87820b1b3055afd5858aee0a843
longhoang94 <longsutls@gmail.com>
2018-09-23 21:18:18 +0700

    refactor code

--- src/PhoneNumberExample.java
+++ src/PhoneNumberExample.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberExample {
-    private static final String PHONE_NUMBER_REGEX = "\\([0-9]{2}\\)\\-\\([0-9]{10,11}\\)";
+    private static final String PHONE_NUMBER_REGEX = "\\([\\d]{2}\\)\\-\\([\\d]{10,11}\\)";
     private static Pattern pattern;
     private static Matcher matcher;
 
